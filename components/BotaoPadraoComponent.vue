c711646104fa3290b1d661556b0d13d52630aff2
micael <micaelxv1@gmail.com>
2024-05-10 18:18:46 -0300

    vue3

---
+++ components/BotaoPadraoComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-btn
+      :text="text"
+      @click="emitirEvento"
+  />
+  
+  <!-- @click="dialog = !dialog" -->
+      
+</template>
+
+<script>
+  export default {
+    name: 'BotaoPadraoComponent', 
+
+    props: {
+      text: {
+        type: String
+      },
+
+      dialog: {
+        type: Boolean,
+      },
+    },
+    data() {
+      return {
+
+      };
+    },
+
+    methods: {
+      emitirEvento(){
+        this.$emit('clicouBotao')
+      }
+    },
+  }
+</script>
---
+++ pages/x.vue
@@ -0,0 +1,46 @@
+<template>
+  <body style="height: 100%;background-color: black">
+    <v-container>
+      <v-row style="width: 100%;" no-gutters align="center">
+        <v-col>
+          <img src="https://s2-techtudo.glbimg.com/f5K6FXnCmZnRvij2SNsrZSn9Iso=/600x0/filters:quality(70)/https://i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/e/D/6e24vbQSaRBG2ScINKIQ/imagem-6.jpg" alt="imagem X">
+        </v-col>
+
+        <v-col class="text-end">
+          <v-container>
+            <h1>Twitter</h1>
+          </v-container>  
+
+          <v-container>
+            <h3>inscrever-se hoje</h3>
+          </v-container>
+            <v-container>
+              <v-btn>
+                <v-icon class="ml-n3 mr-4">
+                  mdi-google
+                </v-icon>
+                <p>inscreva-se pelo google</p>
+              </v-btn>
+            </v-container>
+
+            <v-container>
+              <v-row class="mr-n7">
+                <v-col style class="ml-3 mr-4">
+                  <v-btn width="280">
+                    <v-icon class="ml-n9 mr-4">
+                      mdi-apple
+                    </v-icon>
+                    <p>inscreva-se com Apple</p>
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-container>
+
+        </v-col>
+
+      </v-row>
+    </v-container>
+  </body>  
+</template>
+
+
