f67b0f29d7a342d7ca3b9bd55feba9b302551a33
micael <micaelxv1@gmail.com>
2024-05-21 18:16:42 -0300

    nuxt

--- pages/tabela.vue
+++ pages/tabela.vue
@@ -1,45 +1,43 @@
 <template>
   <v-container style="justify-content: center; border-radius: 25px;">
-    <!-- <h1 style="color: white;">Elementos</h1> -->
     <v-row>
       <v-col>
-        <TabelaComponent @criar="dialog = true" titulo="Elementos" :headers="headers" :items="items" @excluir="deleteItem"
-          @editar="editItem" />
+        <TabelaComponent @criar="dialog = true" titulo="Elementos" :headers="headers" :items="items"
+          @excluir="deleteItem" @editar="editItem" @persist="persist" />
       </v-col>
     </v-row>
-    <v-dialog v-model="dialog" width="auto">
+    <v-dialog v-model="dialog" width="auto" @click:outside="resetDialog">
       <v-card max-width="100%" title="Colocar elementos" theme="dark">
-        <template #actions>
-          <v-form>
-            <v-container>
-              <v-row>
-                <v-col cols="12" sm="15">
-                  <v-text-field label="Nome do produto" v-model="elemento.name" enable />
-                </v-col>
+        <v-form>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="15">
+                <v-text-field label="Nome do produto" v-model="elemento.name" enable />
+              </v-col>
 
-                <v-col cols="12" sm="15">
-                  <v-text-field label="Descrição" v-model="elemento.descricao"  />
-                </v-col>
+              <v-col cols="12" sm="15">
+                <v-text-field label="Descrição" v-model="elemento.descricao" />
+              </v-col>
 
-                <v-col cols="12" sm="15">
-                  <v-text-field label="Imagem" v-model="elemento.img" />
-                </v-col>
+              <v-col cols="12" sm="15">
+                <v-text-field label="Imagem" v-model="elemento.img" />
+              </v-col>
+
+              <v-col cols="12" sm="15">
+                <v-text-field label="publico" v-model="elemento.publico" />
+              </v-col>
+
+              <v-col>
+                <v-autocomplete v-model="elemento.idUsuario" :items="usuarios" item-title="firstname"
+                  item-value="id" />
                 <v-col>
-                  <v-autocomplete
-                  v-model="elemento.idElemento"
-                  :items="eventos"
-                  item-title="usuario"
-                  item-value="id"
-                  />
-                  <v-col>
-                    <v-btn class="ms-auto justify-center d-flex align-center" style="background-color: crimson;"
-                      text="Salvar" @click="dialog = false" />
-                  </v-col>
+                  <v-btn class="ms-auto justify-center d-flex align-center" style="background-color: crimson;"
+                    text="salvar" @click="persist()" />
                 </v-col>
-              </v-row>
-            </v-container>
-          </v-form>
-        </template>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-form>
       </v-card>
     </v-dialog>
   </v-container>
@@ -47,13 +45,10 @@
 
 <script>
 export default {
-  data: () => {
+  data() {
     return {
       dialog: false,
-      ativo: true,
       loading: true,
-      textoUsuario: null,
-      valor: 0,
       elemento: {
         id: null,
         name: null,
@@ -62,7 +57,6 @@ export default {
         publico: null,
         idUsuario: null,
       },
-
       headers: [
         {
           title: 'Identificação',
@@ -87,14 +81,33 @@ export default {
         }
       ],
       items: [],
-      idUsuario: [],
+      usuarios: [],
+    }
+  },
+  watch: {
+    ativo(valor) {
+      if (valor == false) {
+        this.resetElemento()
+      }
     }
   },
   async created() {
     await this.getItens();
+    await this.getUsuario();
   },
-
   methods: {
+    resetElemento() {
+      this.elemento = {
+        id: null,
+        name: null,
+        descricao: null,
+        img: null,
+        publico: null,
+        idUsuario: null,
+      }
+      this.ativo = false;
+    },
+
     async getItens() {
       const response = await this.$api.get('/elemento')
       this.items = response.data
@@ -107,26 +120,41 @@ export default {
           alert(response.message);
         }
       }
-      await this.getItens();
+      await this.getItems();
     },
     editItem(item) {
       this.elemento = { ...item }
       this.dialog = true;
     },
-    async getElemento() {
-      const response = await this.$api.get('/elemento');
-      this.elemento = response.data;
-    },
-
     async persist() {
-      if (this.atividade.id) {
+      if (this.elemento.id) {
         const response = await this.$api.post(`/elemento/persist/${this.elemento.id}`, this.elemento);
       } else {
         const response = await this.$api.post('/elemento/persist', this.elemento);
       }
-      this.resetelemento()
+      this.resetElemento()
       await this.getItems();
     },
+    async getUsuario() {
+      const response = await this.$api.get('/usuario')
+      this.usuarios = response.data;
+      this.loading = false;
+    },
+    async getItems() {
+      const response = await this.$api.get('/elemento');
+      this.items = response.data;
+      this.loading = false;
+    },
+    resetDialog() {
+      this.dialog = false;
+      this.elemento = {
+        id: null,
+        name: null,
+        descricao: null,
+        img: null,
+        idUsuario: null,
+      };
+    }
   },
 }
 </script>
